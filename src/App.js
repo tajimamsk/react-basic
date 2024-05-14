d3491a0cf1ffbae9ce584bc9d525db5f8c620b57
tajimamsk <masaki.tajima@g-seed.co.jp>
2024-05-14 15:26:40 +0900

    useStateまで

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Basic1 from "./components/Basic1";
+import Basic2 from "./components/Basic2";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Basic1 name="Hooks" />
-        <Basic1 name="Today" />
+        {/* <Basic1 name="Hooks" /> */}
+        {/* <Basic1 name="Today" /> */}
+        <Basic2 name="Hooks" />
       </header>
     </div>
   );
