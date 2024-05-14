39abe105168daa7da695710734d002cdc18c3792
tajimamsk <masaki.tajima@g-seed.co.jp>
2024-05-14 14:43:39 +0900

    propsまで

---
+++ src/components/Basic1.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Basic1 = (props) => {
+  const clickHandler = () => {
+    console.log("clicked");
+  };
+  return (
+    <>
+      <button onClick={clickHandler}>Click</button>
+      <h1>Hello {props.name}</h1>
+      <h1>Hello React2</h1>
+    </>
+  );
+};
+
+export default Basic1;
